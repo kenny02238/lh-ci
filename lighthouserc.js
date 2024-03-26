26bb0defcd5257e6b07e290c125743489abe17a4
Kenny <kenny02238@yahoo.com.tw>
2024-03-26 11:29:07 +0800

    test server

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,13 +3,17 @@ module.exports = {
     collect: {
       // collect options here
       numberOfRuns: 1,
+      startServerReadyPattern: "Local",
       startServerCommand: "yarn dev",
       url: ["http://localhost:3000/"],
     },
 
     upload: {
       // upload options here
-      target: "temporary-public-storage",
+      // target: "temporary-public-storage",
+      target: "lhci",
+      serverBaseUrl: "http://localhost:9001",
+      token: "f5333ee7-f7d8-452e-8cda-cd736e48a15f", // build token from lhci wizard
     },
     server: {
       // server options here
